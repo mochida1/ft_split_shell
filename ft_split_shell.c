a23e5cfc35fafbe102ebd17ced8376071d88fb05
Mochida <mochida@42>
2022-08-27 06:09:47 +0200

    todo(bug MALUCO): bug arbitrário com writes e reads estranhos

--- ft_split_shell.c
+++ ft_split_shell.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 01:58:18 by coder             #+#    #+#             */
-/*   Updated: 2022/08/27 03:51:46 by coder            ###   ########.fr       */
+/*   Updated: 2022/08/27 06:06:51 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,47 @@
 
 typedef struct s_split_shell
 {
-
+	char	*string;
+	char	*temp;
+	int		words;
+	int		i;
+	int		split_number;
+	char	delimiter;
 }	t_split_shell;
 
-void	iterate_through_spaces(char *str)
-{
-	while (*str == ' ')
-		str++;
-}
-
-int	iterate_through_quotes(char *str)
+void	iterate_through_quotes(t_split_shell *this)
 {
 	char first_char;
 
-	first_char = *str;
-	if (!first_char) //em caso de erro, volta 0;
-		return (0);
-	if (str[1]) //checa se o proximo elemento não é nulo
-		str++;
+	first_char = this->temp[0];
 	if (first_char == '\'' || first_char == '\"') //se o primeiro caractere for alguma aspa
 	{
-		while (*str && (*str != first_char)) //itera até achar o próximo
-			str++;
-		if (*str == first_char) //quer dizer que temos uma palavra nova;
-			return (1); //retorna 1 para somar a palavra;
+		if (this->temp[1]) //checa se o proximo elemento não é nulo
+			this->temp++;
+		while (this->temp[0] && (this->temp[0] != first_char)) //itera até achar o próximo
+			this->temp++;
+		if (this->temp[0] == first_char)
+			this->temp++;
+		this->words++;
+		return ;
 	}
-	while (*str && (*str != ' ')) //aqui checamos o caso da palavra não conter aspas;
-	{
-		str++;
-	}
-	return (1); //contamos uma palavra a mais
+	while (this->temp[0] && (this->temp[0] != this->delimiter)) //aqui checamos o caso da palavra não conter aspas;
+		this->temp++;
+	this->words++; //contamos mais uma palavra
 }
 
-int	count_words (char *str, char delimiter) //conta quantas palavras temos
+void	count_words (t_split_shell *this) //conta quantas palavras temos
 {
-	char	*temp; //aqui usamos temp para mantér o ponteiro original no lugar certo
-	int		words = 0;
-	while (*temp)
+	this->temp = this->string;
+	while (this->temp[0])
 	{
-		iterate_through_spaces(temp); //pula os espaços
-		words += iterate_through_quotes(temp); //pula até os proximos espaços, se alguma aspas for o primeiro caratere, itera até o primeio espaço depois de aspas
+		while (this->temp[0] == this->delimiter)
+			this->temp++;
+		iterate_through_quotes(this); //pula até os proximos espaços, se alguma aspas for o primeiro caratere, itera até o primeio espaço depois de aspas
 	}
-	return (words);
 }
 
-int	get_word_size (char *str, ' ')
+int	get_word_size (char *str, char delimiter)
 {
 	char	*temp; //não queremos mexer no original;
 	char	first_char;
@@ -79,40 +75,89 @@ int	get_word_size (char *str, ' ')
 	if (first_char == '\'' || first_char == '\"') //se o primeiro char for aspas
 	{
 		temp++; //pula a primeira letra que sabemos qual é
-		i++; // já soma a bagaça
+		size++; // já soma a bagaça
 		while(*str && (*str != first_char)) //enquanto não for a mesma coisa e enquanto nao acabar
 		{
 			temp++;
-			i++;
+			size++;
 		}
-		return (i + (*str == first_char)); //retorna e i, e se o ultimo caractere for igual a ' ou ", soma mais um pq não iteramos por ele
+		return (size + (*str == first_char)); //retorna e i, e se o ultimo caractere for igual a ' ou ", soma mais um pq não iteramos por ele
 	}
-	while (*temp && (temp != ' '))
+	while (*temp && (*temp != delimiter))
 	{
 		temp++;
+		size++;
+	}
+	return (size); // se não for uma palavra que comeca com aspas, simplesmente iteramos pela palavra inteira.
+}
+
+void	copy_to_split(char *split, t_split_shell *this)
+{
+	char	first_char;
+	int		i;
+
+	i = 0;
+	first_char = this->temp[0];
+	if (first_char == '\'' || first_char == '\"')
+	{
+		split[i++] = first_char;
+		this->temp++;
+		while (this->temp[0] != first_char)
+		{
+			split[i] = this->temp[0];
+			printf (">%d<%c\n", i, split[i]);
+			i++;
+			this->temp++;
+		}
+		this->temp++;
+		split[i] = first_char;
+		return ;
+	}
+	while (this->temp[0] && this->temp[0] != ' ')
+	{
+		split[i] = this->temp[0];
+		this->temp++;
 		i++;
 	}
-	return (i); // se não for uma palavra que comeca com aspas, simplesmente iteramos pela palavra inteira.
+	split[i] = 0;
 }
 
 int main (void)
 {
 	char	delimiter = ' ';
-	char	*str = "<< hi this is \" a test<<t\'o>>test\" >><\'< \">\'>|>> \'>>\'<<>>";
+	// char	*str = "<< hi this is \" a test<<t\'o>>test\" >><\'< \">\'>|>> \'>>\'<<>>";
+	char	*str = strdup("    aaaaa    \'bbbbbbb\'   \"ccccccc\" \'dd\"dd\'      \"ee\'ee\" ffffff g");
 
-	char	**splits;
-	int		split_number;
-	int		i;
-	char	*temp;
+	char			**splits;
+	t_split_shell	*this;
 
-	temp = str;
-	split split_number = count_words(str, delimiter);
-	splits = malloc ((split_number + 1) * sizeof(char *)); //allocamos memória para a quantidade de palavras e um byte nulo;
-	while (i < split_number)
+	this = calloc (1, sizeof(*this) + 1);
+	this->temp = str;
+	this->delimiter = delimiter;
+	this->string = str;
+	count_words(this);
+	splits = calloc ((this->words + 1) , sizeof(char *)); //allocamos memória para a quantidade de palavras e um byte nulo;
+	this->temp = this->string;
+	while (this->i < this->words)
 	{
-		iterate_through_spaces(temp); //vai até a primeira/proxima palavra;
-		split[i] = calloc ((get_word_size(temp)) + 1), sizeof(char)); //alloca memória pro rolê, já inicializada em 0 :)
-		copy_to_split (temp, split[i]);
-		i++;
+		while (this->temp[0] == this->delimiter) //vai até a primeira/proxima palavra;
+			this->temp++;
+		splits[this->i] = calloc ((get_word_size(this->temp, this->delimiter) + 1) , sizeof(char)); //alloca memória pro rolê, já inicializada em 0 :)
+		copy_to_split (splits[this->i], this);
+		printf ("[%d]%s\n", this->i, splits[this->i]);
+		this->i++;
 	}
+
+
+	this->i = 0;
+	{
+		while (this->i < this->words)
+		{
+			free (splits[this->i]);
+			this->i++;
+		}
+	}
+	free (this);
+	free (str);
+	return (0);
 }
